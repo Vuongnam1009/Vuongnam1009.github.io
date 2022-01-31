965d9b1f016afcf8520c92be9626e074d82f9acb
Nam <vuongnam1009@gmail.com>
2022-01-31 17:14:05 +0700

    Signed-off-by: Nam <vuongnam1009@gmail.com>

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'chao lpa',
+    introDesc: `Các cụ đã nói rồi:
+    Chọn xoài đừng để xoài chua
+    Chọn bạn đừng để xoài chua chọn bạn`,
+    btnIntro: '',
+    title: 'm có muốn nghe cmsn kh',
+    desc: 'chọn khôn nha ',
+    btnYes: 'có',
+    btnNo: 'kó',
+    question:'Trên thế giới hơn 7 tỉ người mà sao t lại gặp m???',
+    btnReply: '<3333',
+    reply: '',
+    mess: 'chúc lpa xinh tươi tuổi mới luôn xink điẹp, bớt trtr, nhiều tiền đặt sốp bi, có mối tình đầu chất cả lượng. chúc lpa tất cả nhoa moa moa',
+    messDesc: 'mình cần 1 kèo đi ăn',
+    btnAccept: '',
+    messLink: 'https://www.messenger.com/t/4053532394683940/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
